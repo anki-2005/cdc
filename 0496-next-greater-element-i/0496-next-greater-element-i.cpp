842d6c6c8d55d041604d99de9a5296e5027aa6d2
anki-2005 <ankulmishra720@gmail.com>
2025-11-18 14:59:37 +0530

    Time: 1 ms (54.05%), Space: 13 MB (14.15%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int>mpp;
+        stack<int>st;
+        int n=nums2.size();
+        for(int i=n-1;i>=0;i--){
+            if(st.empty()){
+               mpp[nums2[i]]=-1;
+               st.push(nums2[i]);
+            }
+            else {
+               while(!st.empty() && st.top()<nums2[i]) {
+                st.pop();
+               }
+               if(st.empty()) mpp[nums2[i]]=-1;
+               else mpp[nums2[i]]=st.top();
+               st.push(nums2[i]);
+            }
+        }
+        vector<int>ans;
+        for(auto it:nums1){
+            auto k=mpp.find(it);
+            if(k!=mpp.end()) ans.push_back(k->second);
+            else continue;
+        }
+        return ans;
+    }
+};
