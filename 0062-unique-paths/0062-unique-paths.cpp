c8e6af257fda2b7f56e9126b90c51a446f177327
anki-2005 <ankulmishra720@gmail.com>
2025-11-26 15:12:06 +0530

    Time: 2 ms (19.11%), Space: 9.4 MB (47.78%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    int noofpath(int m,int n,vector<vector<int>>&cache){
-    if(cache[m][n]!=-1) return cache[m][n];
-    if(m==0 && n==0) cache[m][n]= 1;
-    if(m==0 || n==0) cache[m][n]=1;
-    else{
-    int a=noofpath(m-1,n,cache);
-    int b=noofpath(m,n-1,cache);
-    cache[m][n]=a+b;
-    }
-    return cache[m][n];
+    int noofpath(int m, int n, vector<vector<int>> &dp){
+        if(m<0 || n<0) return 0;
+        if(m==0 && n==0) return 1;
+         if(dp[m][n]!=-1) return dp[m][n];
+        int a=noofpath(m-1,n,dp);
+        int b=noofpath(m,n-1,dp);
+        return dp[m][n]=a+b;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>cache(m,vector<int>(n,-1));
-        int ans=noofpath(m-1,n-1,cache);
-        return ans;
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+         return noofpath(m-1,n-1,dp);
     }
 };
