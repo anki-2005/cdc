ef7f65347901e723ea62bfaf2b5b5a89141167e0
anki-2005 <ankulmishra720@gmail.com>
2025-12-03 10:16:39 +0530

    Time: 133 ms (30.29%), Space: 174 MB (30.05%) - LeetHub

--- 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.cpp
+++ 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.cpp
@@ -6,7 +6,7 @@ public:
         int n=nums.size();
         if(k==0) return n;
         for(int i=0;i<n-1;i++){
-            int greaterIdx = upper_bound(nums.begin(), nums.end(), nums[i]) - nums.begin();
+            int greaterIdx = upper_bound(nums.begin(), nums.end(), nums[i])-nums.begin();
             int greaterCount = n - greaterIdx;
             if(greaterCount >= k) cnt++;
         }
