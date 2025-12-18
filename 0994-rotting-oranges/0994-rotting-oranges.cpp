56ceb705ca9ad947c02f9984508053dc049e2a91
anki-2005 <ankulmishra720@gmail.com>
2025-12-18 20:38:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,59 +1,53 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
+        int n = grid.size();
         queue<pair<int,int>>q;
-        int fresh=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==2){
+        int count=0;
+        for(int i=0;i< n;i++){
+            for(int j=0; j<n;j++){
+                if(grid[i][j]==1) count++;
+                else if(grid[i][j]==2) {
                     q.push({i,j});
                 }
-                if(grid[i][j]==1) fresh++;
             }
         }
-        if(fresh==0) return 0;
-         int maxtime = -1;
+        if(count==0) return 0;
+        int time=-1;
         while(!q.empty()){
             int s=q.size();
-            maxtime++;
-            for(int i=0;i<s;i++){
-                auto p=q.front();
+            time++;
+            for(int x=0;x<s;x++){
+                pair<int,int>p = q.front();
                 q.pop();
-                int x=p.first;
-                int y=p.second;
-                // left
-                if(y - 1 >= 0 && grid[x][y - 1] == 1){
-                    grid[x][y - 1] = 2;
-                    fresh--;
-                    q.push({x, y - 1});
+                int i=p.first;
+                int j=p.second;
+                // up
+                if(i-1>=0 && grid[i-1][j]==1){
+                    count--;
+                    grid[i-1][j]=2;
+                    q.push({i-1,j});
                 }
-
-                // RIGHT
-                if(y + 1 < n && grid[x][y + 1] == 1){
-                    grid[x][y + 1] = 2;
-                    fresh--;
-                    q.push({x, y + 1});
+                // down 
+                 if(i+1<n && grid[i+1][j]==1){
+                    count--;
+                    grid[i+1][j]=2;
+                    q.push({i+1,j});
                 }
-
-                // UP
-                if(x - 1 >= 0 && grid[x - 1][y] == 1){
-                    grid[x - 1][y] = 2;
-                    fresh--;
-                    q.push({x - 1, y});
+                // right
+                 if(j+1<n && grid[i][j+1]==1){
+                    count--;
+                    grid[i][j+1]=2;
+                    q.push({i,j+1});
                 }
-
-                // DOWN
-                if(x + 1 < m && grid[x + 1][y] == 1){
-                    grid[x + 1][y] = 2;
-                    fresh--;
-                    q.push({x + 1, y});
+                if(j-1 >=0 && grid[i][j-1]==1){
+                    count--;
+                    grid[i][j-1]=2;
+                    q.push({i,j-1});
                 }
-
-                }
-    
             }
-            return (fresh==0 )? maxtime  : -1; 
         }
+        if(count==0) return time;
+        else return -1 ;
+    }
 };
