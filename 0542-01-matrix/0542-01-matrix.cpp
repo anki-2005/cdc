6ff8d075a3074bff9b4111257eef0979e4cc28b9
anki-2005 <ankulmishra720@gmail.com>
2026-01-23 09:37:35 +0530

    Time: 15 ms (72.29%), Space: 39 MB (21.14%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n= mat.size();
+        queue<pair<pair<int,int>,int>>q;
+        int n=mat.size();
         int m=mat[0].size();
         vector<vector<int>>ans(n,vector<int>(m,0));
         vector<vector<int>>visited(n,vector<int>(m,0));
-        queue<pair<pair<int,int>,int>>q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(mat[i][j]==0) {
+                if(mat[i][j]==0){
                     q.push({{i,j},0});
                     visited[i][j]=1;
                 }
@@ -17,29 +17,26 @@ public:
         while(!q.empty()){
             auto p=q.front();
             q.pop();
-            auto l=p.first;
+            int i=p.first.first;
+            int j=p.first.second;
             int dist=p.second;
-            int i=l.first;
-            int j=l.second;
+           
             ans[i][j]=dist;
-            int count=dist+1;
-            if(i-1>=0 && visited[i-1][j]==0){
-                    visited[i-1][j]=1;
-                    q.push({{i-1,j},count});
-                }
-                // down 
-             if(i+1<n && visited[i+1][j]==0){
-                    visited[i+1][j]=1;
-                    q.push({{i+1,j},count});
-                }
-                // right
-             if(j+1<m && visited[i][j+1]==0){
-                    visited[i][j+1]=1;
-                    q.push({{i,j+1},count});
-                }
-            if(j-1 >=0 && visited[i][j-1]==0){
-                    visited[i][j-1]=1;
-                    q.push({{i,j-1},count});
+            if(i-1>=0 && visited[i-1][j]!=1){
+                 visited[i-1][j]=1;
+                q.push({{i-1,j},dist+1});
+            }
+            if(i+1<n && visited[i+1][j]!=1){
+                 visited[i+1][j]=1;
+                q.push({{i+1,j},dist+1});
+            }
+            if(j-1>=0 && visited[i][j-1]!=1){
+                 visited[i][j-1]=1;
+                q.push({{i,j-1},dist+1});
+            }
+            if(j+1<m && visited[i][j+1]!=1){
+                 visited[i][j+1]=1;
+                q.push({{i,j+1},dist+1});
             }
         }
         return ans;
